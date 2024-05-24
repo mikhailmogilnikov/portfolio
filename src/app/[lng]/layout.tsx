1517bbf2de57003877bfccaf8163e933f77bcc2a
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-05-24 14:14:34 +0300

    fix: favicons paths

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -51,29 +51,19 @@ export default function RootLayout({ children, params: { lng } }: PageProps) {
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/assets/favicon/apple-touch-icon.png'
+          href='/apple-icon?assets/favicon/apple-touch-icon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/assets/favicon/favicon-32x32.png'
+          href='/icon?assets/favicon/favicon-32x32.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/assets/favicon/favicon-16x16.png'
-        />
-        <link
-          rel='mask-icon'
-          href='/assets/favicon/safari-pinned-tab.svg'
-          color='#000000'
-        />
-        <meta name='msapplication-TileColor' content='#c2dbff' />
-        <meta
-          name='msapplication-config'
-          content='/assets/favicon/browserconfig.xml'
+          href='/icon?assets/favicon/favicon-16x16.png'
         />
         <meta name='theme-color' content='#000000' />
       </head>
