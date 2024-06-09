2bef01197097b6ff31db6395d834808c7c92f369
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-06-09 16:54:48 +0300

    feat: suspense & preload to a video component

--- src/entities/gallery/ui/video.tsx
+++ src/entities/gallery/ui/video.tsx
@@ -1,15 +1,21 @@
 import { TGalleryItem } from '@mikhailmogilnikov/shared/model/types/project.type';
+import { Skeleton } from '@nextui-org/skeleton';
+import { Suspense } from 'react';
 
 export const Video = ({ url }: Pick<TGalleryItem, 'url'>) => {
   return (
-    <video
-      loop
-      autoPlay
-      playsInline
-      muted
-      controls={false}
-      src={url}
-      className='w-full h-full snap-start flex-shrink-0 object-cover'
-    />
+    <Suspense fallback={<Skeleton className='w-full h-full' />}>
+      <video
+        loop
+        autoPlay
+        playsInline
+        muted
+        controls={false}
+        src={url}
+        preload='auto'
+        aria-label='Video player'
+        className='w-full h-full snap-start flex-shrink-0 object-cover'
+      />
+    </Suspense>
   );
 };
