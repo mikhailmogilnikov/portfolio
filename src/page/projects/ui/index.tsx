e3bb2f44dc41c0ce8c76389ee950f9d84664c236
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-06-15 23:03:38 +0300

    fix: remove dynamic in projects list

--- src/page/projects/ui/index.tsx
+++ src/page/projects/ui/index.tsx
@@ -1,23 +1,14 @@
-import dynamic from 'next/dynamic';
+import { ProjectsList } from '@mikhailmogilnikov/widgets/projects-list';
 
 type Props = {
   lng: string;
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const DynamicProjectsList = dynamic(() =>
-  import('@mikhailmogilnikov/widgets/projects-list').then(
-    (mod) => mod.ProjectsList,
-  ),
-);
-
 export const ProjectsPage = ({ lng, searchParams }: Props) => {
   const { project } = searchParams;
 
   return (
-    <DynamicProjectsList
-      lng={lng}
-      activeProject={project as string | undefined}
-    />
+    <ProjectsList lng={lng} activeProject={project as string | undefined} />
   );
 };
