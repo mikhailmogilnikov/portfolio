0a74d6684710e5257907f8968898b25f8a4d61c5
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-06-17 00:00:30 +0300

    fix: video preload

--- src/entities/gallery/ui/video.tsx
+++ src/entities/gallery/ui/video.tsx
@@ -1,66 +1,11 @@
 import { TGalleryItem } from '@mikhailmogilnikov/shared/model/types/project.type';
 import { m, useInView } from 'framer-motion';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { TbPhotoVideo } from 'react-icons/tb';
 
 export const Video = ({ url }: Pick<TGalleryItem, 'url'>) => {
   const ref = useRef<HTMLVideoElement>(null);
   const isInView = useInView(ref);
-  const [videoSrc, setVideoSrc] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-  const controllerRef = useRef<AbortController | null>(null);
-
-  useEffect(() => {
-    const fetchVideo = async () => {
-      if (loading || videoSrc) return;
-
-      const controller = new AbortController();
-      controllerRef.current = controller;
-      setLoading(true);
-
-      try {
-        const response = await fetch(url, {
-          headers: {
-            Range: 'bytes=0-',
-          },
-          signal: controller.signal,
-        });
-
-        if (response.ok) {
-          const blob = await response.blob();
-          const videoUrl = URL.createObjectURL(blob);
-          setVideoSrc(videoUrl);
-        } else {
-          console.error(
-            'Ошибка при загрузке видео:',
-            response.status,
-            response.statusText,
-          );
-        }
-      } catch (error: any) {
-        if (error.name === 'AbortError') {
-          console.log('Загрузка видео была отменена');
-        } else {
-          console.error('Ошибка при загрузке видео:', error);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (isInView) {
-      fetchVideo();
-    } else if (controllerRef.current) {
-        controllerRef.current.abort();
-        setLoading(false);
-      }
-
-    return () => {
-      if (controllerRef.current) {
-        controllerRef.current.abort();
-      }
-    };
-  }, [isInView, url]);
 
   useEffect(() => {
     const videoElement = ref.current;
@@ -72,7 +17,7 @@ export const Video = ({ url }: Pick<TGalleryItem, 'url'>) => {
     } else {
       videoElement.pause();
     }
-  }, [isInView, videoSrc]);
+  }, [isInView]);
 
   return (
     <div className='w-full h-full relative flex items-center justify-center'>
@@ -86,7 +31,7 @@ export const Video = ({ url }: Pick<TGalleryItem, 'url'>) => {
         muted
         preload='none'
         controls={false}
-        src={videoSrc}
+        src={url}
         className='absolute w-full h-full snap-start flex-shrink-0 object-cover z-10'
       />
       <TbPhotoVideo className='animate-pulse z-0 w-1/4 h-1/4' />
