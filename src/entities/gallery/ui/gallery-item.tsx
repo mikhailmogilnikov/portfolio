b15d841a017b246e8142db58d9c211e7f845c9a0
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-09-12 23:07:25 +0300

    fix: disable append animation in gallery

--- src/entities/gallery/ui/gallery-item.tsx
+++ src/entities/gallery/ui/gallery-item.tsx
@@ -1,5 +1,4 @@
 import { TGalleryItem } from '@mikhailmogilnikov/shared/model/types/project.type';
-import { AppendAnimation } from '@mikhailmogilnikov/shared/ui/(layout)/append-animation';
 import { useLightbox } from '@mikhailmogilnikov/shared/lib/hooks/use-lightbox';
 import { m } from 'framer-motion';
 import { Text } from '../../../shared/ui/(layout)/text';
@@ -15,28 +14,28 @@ export const GalleryItem = ({ item, imgSizes }: Props) => {
   const { aspect, description } = item;
 
   return (
-    <AppendAnimation>
-      <button
-        type='button'
-        onClick={() => setGallery([item])}
-        className='w-full h-fit flex flex-col gap-3 md:gap-4'
+    // <AppendAnimation>
+    <button
+      type='button'
+      onClick={() => setGallery([item])}
+      className='w-full h-fit flex flex-col gap-3 md:gap-4'
+    >
+      <m.div
+        layoutId={`${item.url}_item`}
+        style={{ aspectRatio: aspect }}
+        className='w-full bg-default rounded-2xl relative shadow-base border-1 border-divider overflow-clip'
       >
-        <m.div
-          layoutId={`${item.url}_item`}
-          style={{ aspectRatio: aspect }}
-          className='w-full bg-default rounded-2xl relative shadow-base border-1 border-divider overflow-clip'
+        <Media media={item} imgSizes={imgSizes} />
+      </m.div>
+      {description && (
+        <Text
+          className='text-sm md:text-medium font-medium text-start'
+          opacity={0.8}
         >
-          <Media media={item} imgSizes={imgSizes} />
-        </m.div>
-        {description && (
-          <Text
-            className='text-sm md:text-medium font-medium text-start'
-            opacity={0.8}
-          >
-            {description}
-          </Text>
-        )}
-      </button>
-    </AppendAnimation>
+          {description}
+        </Text>
+      )}
+    </button>
+    // </AppendAnimation>
   );
 };
